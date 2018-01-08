270eae23f90e7872d0bf4d65444097b4f5199359
jameswquinn <jameswquinn@users.noreply.github.com>
2018-01-08 12:10:02 +0000

    update

--- gulpfile.js
+++ gulpfile.js
@@ -62,15 +62,52 @@ gulp.task('sass',  () => {
 
 });
 
-
 // Generate & Inline Critical-path CSS
 gulp.task('critical',  (cb) => {
     critical.generate({
-        inline: true,
-        src: 'index.html',
-        dest: 'index-critical.html',
-        width: 320,
-        height: 480,
-        minify: true
+			// Inline the generated critical-path CSS
+			    // - true generates HTML
+			    // - false generates CSS
+			    inline: true,
+
+			    // Your base directory
+			    //base: 'dist/',
+
+			    // HTML source
+			    //html: '<html>...</html>',
+
+			    // HTML source file
+			    src: 'index.html',
+
+			    // Your CSS Files (optional)
+			    //css: ['dist/styles/main.css'],
+
+			    // Viewport width
+			    width: 1300,
+
+			    // Viewport height
+			    height: 900,
+
+			    // Target for final HTML output.
+			    // use some CSS file when the inline option is not set
+			    dest: 'index-critical.html',
+
+			    // Minify critical-path CSS when inlining
+			    minify: true,
+
+			    // Extract inlined styles from referenced stylesheets
+			    //extract: true,
+
+			    // Complete Timeout for Operation
+			    timeout: 30000,
+
+			    // Prefix for asset directory
+			    //pathPrefix: '/MySubfolderDocrot',
+
+			    // ignore CSS rules
+			    ignore: ['font-face',/some-regexp/],
+
+			    // overwrite default options
+			    ignoreOptions: {}
     });
 });
