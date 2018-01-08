b2b0394b050674844c031b148a09fc693f3bf443
jameswquinn <jameswquinn@users.noreply.github.com>
2018-01-08 13:22:58 +0000

    update

--- gulpfile.js
+++ gulpfile.js
@@ -76,6 +76,104 @@ gulp.task('sass', () => {
 
 });
 
+/**
+ * Task: build:scripts:core
+ *
+ * Concatenates and uglifies global JS files and outputs result to the
+ * appropriate location.
+ */
+
+
+gulp.task('build:scripts:core', ['build:scripts:vendor'], () =>
+	gulp.src(['node_modules/axios/dist/axios.js',
+  'node_modules/moonjs/dist/moon.js',
+  'node_modules/moon-router/dist/moon-router.js',
+  'node_modules/monx/dist/monx.js',
+  'node_modules/lazysizes/lazysizes.js',
+])
+		.pipe($.sourcemaps.init({largeFile: true}))
+		.pipe($.concat({path: 'js/core.js', cwd: ''}))
+    .pipe($.uglify())
+		.pipe($.rev())
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('dist'))
+);
+
+gulp.task('build:scripts:vendor', () =>
+	gulp.src(['node_modules/axios/dist/axios.js',
+  'node_modules/lazysizes/plugins/bgset/ls.bgset.js',
+  'node_modules/lazysizes/plugins/video-embed/ls.video-embed.js',
+  'node_modules/covervid/covervid.js',
+  'node_modules/macy/dist/macy.js',
+  'node_modules/siema/dist/siema.min.js',
+  'node_modules/instafeed/lib/instafeed.js',
+  'node_modules/dynamics.js/lib/dynamics.js'
+])
+		.pipe($.sourcemaps.init({largeFile: true}))
+		.pipe($.concat({path: 'js/vendor.js', cwd: ''}))
+    .pipe($.uglify())
+		.pipe($.rev())
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('dist'))
+);
+
+/**
+ * Task: build:images
+ *
+ * Optimizes and copies image files.
+ *
+ * Using gulp-responsive
+ *
+ /**
+<picture>
+<source sizes="(max-width: 2048px) 100vw, 2048px"
+srcset="
+image1-100px.webp 100w,
+image1-240px.webp 240w,
+image1-320px.webp 320w,
+image1-500px.webp 500w,
+image1-640px.webp 640w,
+image1-800px.webp 800w,
+image1-1600px.webp 1600w,
+image1-2048px.webp 2048w" type="image/webp">
+<img
+sizes="(max-width: 2048px) 100vw, 2048px"
+srcset="
+image1-100px.jpg 100w,
+image1-240px.jpg 240w,
+image1-320px.jpg 320w,
+image1-500px.jpg 500w,
+image1-640px.jpg 640w,
+image1-800px.jpg 800w,
+image1-1600px.jpg 1600w,
+image1-2048px.jpg 2048w"
+src="image1-640px.jpg"
+alt="" type="image/jpeg">
+</picture>
+ */
+
+ gulp.task('build:images', () => {
+	 gulp.src('src/*.{png,jpg}')
+	.pipe($.responsive({'logo.png': [
+			{
+				width: 200,
+				height: 200,
+				rename: {
+					prefix: "bonjour-",
+					suffix: '_200px',
+					extname: '.jpg',
+				},
+				format: 'jpeg',
+			},{
+				width: 200 * 2,
+				height: 200 * 2,
+				rename: 'logo@2x.png'
+			}
+		]
+}))
+	.pipe(gulp.dest('dist'));
+});
+
 // Generate & Inline Critical-path CSS
 gulp.task('critical', (cb) => {
     critical.generate({
